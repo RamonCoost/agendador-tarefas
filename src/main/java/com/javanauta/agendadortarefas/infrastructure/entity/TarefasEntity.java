23e76964bd275a7411665e69df9121f21d749aa1
RamonCoost <ramoncoost@gmail.com>
2025-08-19 14:33:30 -0300

    adicionado método post

---
+++ src/main/java/com/javanauta/agendadortarefas/infrastructure/entity/TarefasEntity.java
@@ -0,0 +1,29 @@
+package com.javanauta.agendadortarefas.infrastructure.entity;
+
+import com.javanauta.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Document("tarefa")
+public class TarefasEntity {
+
+    @Id
+    private String id;
+    private String nomeTarefa;
+    private String descricao;
+    private String emailUsuario;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataEvento;
+    private LocalDateTime dataAlteracao;
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+
+}
